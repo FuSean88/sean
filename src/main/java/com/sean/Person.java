fed22341971fc2ff7de985a4faa89ab0b070a670
FuSean88 <sean.fu73@gmail.com>
2019-04-05 00:47:16 +0800

    Initial commit

---
+++ src/main/java/com/sean/Person.java
@@ -0,0 +1,7 @@
+package com.sean;
+
+public class Person{
+    public void Hello() {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/kotlin/com/sean/Hello.kt
@@ -0,0 +1,13 @@
+package com.sean
+
+fun main() {
+//    println("Hello kotlin")
+//    Human().hello();
+        val h = Human()
+        h.hello() 
+}
+class Human{
+    fun hello() {
+        println("Hello kotlin")
+    }
+}
